86fb16d5647df02bad70b0491b84695c85cc7446
Nina Siegel <nina.siegel@binary-butterfly.de>
2023-11-03 20:12:23 +0100

    home handler moved

--- internal/handler/home.go
+++ internal/handler/home.go
@@ -1,1 +1,13 @@
 package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (h Handler) Main(w http.ResponseWriter, r *http.Request) {
+	err := h.app.Templates.RenderTemplate(w, "main.tmpl", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
