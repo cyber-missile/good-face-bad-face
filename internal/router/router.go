52085c5936700eecf66e9641ac7d96f22dfbb14e
Nina Siegel <nina.siegel@l1f.de>
2024-01-18 14:09:03 +0100

    fix: handling asstes in a better way, so it can actualy serviced by the server

--- internal/router/router.go
+++ internal/router/router.go
@@ -13,9 +13,25 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func getRoutes(app *application.App) *chi.Mux {
+func fileServer() (*http.Handler, error) {
+	assets, err := web.Assets()
+	if err != nil {
+		return nil, err
+	}
+
+	fileServer := http.FileServer(http.FS(assets))
+
+	return &fileServer, nil
+}
+
+func getRoutes(app *application.App) (*chi.Mux, error) {
 	handlers := handler.New(app)
 	router := chi.NewRouter()
+
+	fileServer, err := fileServer()
+	if err != nil {
+		return nil, err
+	}
 
 	router.Use(middleware.RequestID)
 	router.Use(middleware.RealIP)
@@ -25,20 +41,24 @@ func getRoutes(app *application.App) *chi.Mux {
 	}))
 	router.Use(middleware.Recoverer)
 
-	fileServer := http.FileServer(http.FS(web.StaticFilesDir))
-	router.Handle("/static/*", http.StripPrefix("/static/", fileServer))
+	router.Handle("/static/*", http.StripPrefix("/static/", *fileServer))
 
 	router.Get("/", handlers.Main)
 	router.Get("/game/", handlers.Board)
 	router.Get("/ws", handlers.Websocket)
 
-	return router
+	return router, nil
 }
 
 func Start(app *application.App, ctx context.Context) error {
+	handler, err := getRoutes(app)
+	if err != nil {
+		return err
+	}
+
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%d", app.Config.Port),
-		Handler: getRoutes(app),
+		Handler: handler,
 	}
 
 	app.Logger.Info("Starting http server")
@@ -52,8 +72,6 @@ func Start(app *application.App, ctx context.Context) error {
 
 		close(ch)
 	}()
-
-	var err error
 
 	select {
 	case err = <-ch:
